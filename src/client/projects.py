12b9b6ec41d4c4ba12325518a7f9ae36f6d03c04
Daniela Carrero <danielacarreror@hotmail.com>
2021-06-21 23:15:24 -0300

    env variable

--- src/client/projects.py
+++ src/client/projects.py
@@ -1,11 +1,9 @@
 import httpx
 import os
 
-BASE_URL = os.environ['PROJECTS_BASE_URL']
-
 
 async def getProject(projectId: int):
-    url = f"{BASE_URL}/api/project/{projectId}"
+    url = f"{os.environ['PROJECTS_BASE_URL']}/api/project/{projectId}"
 
     async with httpx.AsyncClient() as client:
         h = {'X-override-token': 'true'}
