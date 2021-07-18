778b8b42400af5c95342400f089e5bb4054dd83e
Daniela Carrero <danielacarreror@hotmail.com>
2021-07-18 20:15:43 -0300

    fix a test

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,13 +7,13 @@ from httpx import AsyncClient
 client = TestClient(app)
 
 review_json = {
-        'reviewerId': 0,
-        'projectId': 1,
-        'id': 0,
-        'status': 'pending',
-        'createdAt': '',
-        'updatedAt': ''
-    }
+    'reviewerId': 0,
+    'projectId': 1,
+    'id': 0,
+    'status': 'pending',
+    'createdAt': '',
+    'updatedAt': ''
+}
 
 project_json = {
     'mediaUrls': [],
@@ -35,9 +35,11 @@ project_json = {
     'updatedAt': ''
 }
 
+
 @pytest.fixture
 def non_mocked_hosts() -> list:
     return ["test"]
+
 
 def test_read_main():
     response = client.get("/")
@@ -46,55 +48,6 @@ def test_read_main():
 
 
 # REVIEWS
-# @pytest.mark.asyncio
-# async def test_post_review(mocker):
-#     review_mock = Review(reviewerId=0,
-#                          projectId=1,
-#                          id=0,
-#                          status='pending',
-#                          createdAt='',
-#                          updatedAt='')
-#
-#     project_mock = Project(mediaUrls=[],
-#                            hashtags=[],
-#                            id=1,
-#                            stages=[],
-#                            title='title',
-#                            description='description',
-#                            category='category',
-#                            status='created',
-#                            fundedAmount=0.0,
-#                            location=None,
-#                            ownerId=3,
-#                            reviewerId=None,
-#                            walletId=None,
-#                            currentStageId=None,
-#                            finishDate='',
-#                            createdAt='',
-#                            updatedAt='')
-#
-#     async def mock_request_review():
-#         return review_response_mock
-#
-#     review_response_mock = ReviewResponseModel(project=project_mock, review=review_mock)
-#     mocker.patch('src.controller.review_controller.request_review', return_value=mock_request_review())
-#
-#     body = {
-#         'reviewerId': 0,
-#         'projectId': 1
-#     }
-#
-#     async with AsyncClient(app=app, base_url="http://test") as ac:
-#         response = await ac.post("/reviews", json=body)
-#
-#     assert response.status_code == 201
-#     assert response.json() == review_response_mock
-#
-#     #response = client.post("/reviews", json=body)
-#     #print(response.text)
-#     #assert response.status_code == 201
-#     #assert response.json() == review_response_mock
-
 @pytest.mark.asyncio
 async def test_post_review(httpx_mock: HTTPXMock):
     body = {
@@ -103,10 +56,12 @@ async def test_post_review(httpx_mock: HTTPXMock):
     }
 
     httpx_mock.add_response(method="POST", url='https://seedy-fiuba-users-api.herokuapp.com/reviews', json=review_json)
-    httpx_mock.add_response(method="PUT", url='https://seedy-fiuba-projects-api.herokuapp.com/api/project/1', json=project_json)
+    httpx_mock.add_response(method="PUT", url='https://seedy-fiuba-projects-api.herokuapp.com/api/project/1',
+                            json=project_json)
 
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.post("/reviews", json=body)
 
     print(response.text)
     assert response.status_code == 201
+    assert response.json() == {'project': project_json, 'review': review_json}
